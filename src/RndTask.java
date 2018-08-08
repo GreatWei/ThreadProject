655b0fe012e20f5fdf3211db905a5840fdd0db88
GreatWei <1121412775@qq.com>
2018-08-08 18:15:03 +0800

    Signed-off-by: GreatWei <1121412775@qq.com>

---
+++ src/RndTask.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+public class RndTask implements Callable<Long> {
+
+	private int mode = 0;
+
+	public RndTask(int mdoe) {
+		// TODO Auto-generated constructor stub
+		this.mode = mdoe;
+	}
+
+	public Random getRandom() {
+		return null;
+	}
+
+	@Override
+	public Long call() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
